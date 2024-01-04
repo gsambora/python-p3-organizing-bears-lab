8dbd74dfbaece6ffd71a3573628b760bc68541fa
Hana <grace.sambora@gmail.com>
2024-01-04 15:29:23 -0600

    Complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 4, 'M', 'Brown', 'Friendly', true);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Rowdy', 1, 'M', 'Black', 'Crazy', false);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Tabitha', 5, 'F', 'Tan', 'Sweetie', true);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 3, 'M', 'Brown', 'Wary', false);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Melissa', 8, 'F', 'Black', 'Silly', true);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Grinch', 2, 'M', 'Green', 'Stingy', true);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Wendy', 1, 'F', 'Tan', 'Friendly', true);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES (NULL, 10, 'F', 'Brown', 'Strange', false);
